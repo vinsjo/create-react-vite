3a2ddf34d717230551401f8fbc50a8d72c5e2126
Vincent Sjögren <vincentsjogren@gmail.com>
2022-09-01 13:58:28 +0200

    initial commit

---
+++ build.config.ts
@@ -0,0 +1,13 @@
+// Based on: https://github.com/vitejs/vite/blob/main/packages/create-vite/build.config.ts
+import { defineBuildConfig } from 'unbuild';
+
+export default defineBuildConfig({
+	entries: ['src/index'],
+	clean: true,
+	rollup: {
+		inlineDependencies: true,
+		esbuild: {
+			minify: true,
+		},
+	},
+});
