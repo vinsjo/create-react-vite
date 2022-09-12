5572dc9f456474c951c64b638e2406839f5dd51e
Vincent Sjögren <vincentsjogren@gmail.com>
2022-09-12 10:51:34 +0200

    added typescript support

---
+++ src/index.ts
@@ -0,0 +1,142 @@
+// Based on (and copied from):
+// https://github.com/vitejs/vite/blob/main/packages/create-vite/src/index.ts
+
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import minimist from 'minimist';
+import prompts from 'prompts';
+import { green, red, reset, yellow } from 'kolorist';
+import {
+	copy,
+	formatTargetDir,
+	emptyDir,
+	isEmpty,
+	isValidPackageName,
+	toValidPackageName,
+} from './utils';
+
+const argv = minimist<{
+	t?: string;
+}>(process.argv.slice(2), { string: ['_'] });
+const cwd = process.cwd();
+
+(async function main() {
+	let targetDir = formatTargetDir(argv._[0]);
+	const defaultTargetDir = 'vite-react-app';
+
+	const getProjectName = () =>
+		!targetDir ? path.basename(path.resolve()) : targetDir;
+
+	let result: prompts.Answers<
+		'projectName' | 'overwrite' | 'packageName' | 'useTypeScript'
+	>;
+	try {
+		result = await prompts(
+			[
+				{
+					type: targetDir ? null : 'text',
+					message: reset('Project name:'),
+					initial: defaultTargetDir,
+					onState: ({ value }) => {
+						targetDir = formatTargetDir(value) || defaultTargetDir;
+					},
+					name: 'projectName',
+				},
+				{
+					type: () =>
+						!targetDir ||
+						!fs.existsSync(targetDir) ||
+						isEmpty(targetDir)
+							? null
+							: 'confirm',
+					message: () =>
+						(targetDir === '.'
+							? 'Current directory'
+							: `Target directory "${targetDir}"`) +
+						` is not empty. Remove existing files and continue? `,
+					name: 'overwrite',
+				},
+				{
+					type: (_, { overwrite }) => {
+						if (overwrite !== false) return null;
+						throw new Error(`${red('✖')} Operation cancelled`);
+					},
+					name: 'overwriteChecker',
+				},
+				{
+					type: () =>
+						isValidPackageName(getProjectName()) ? null : 'text',
+					name: 'packageName',
+					message: reset('Package name:'),
+					initial: () => toValidPackageName(getProjectName()),
+					validate: (dir) =>
+						isValidPackageName(dir) || 'Invalid package.json name',
+				},
+				{
+					type: 'confirm',
+					name: 'useTypeScript',
+					message: 'Use TypeScript? ',
+				},
+			],
+			{
+				onCancel: () => {
+					throw new Error(`${red('✖')} Operation cancelled`);
+				},
+			}
+		);
+	} catch (err: any) {
+		console.log(err.message);
+		return;
+	}
+
+	const { overwrite, packageName, useTypeScript } = result;
+	const root = path.join(cwd, targetDir);
+
+	if (overwrite) {
+		emptyDir(root);
+	} else if (!fs.existsSync(root)) {
+		fs.mkdirSync(root, { recursive: true });
+	}
+
+	console.log(`\nScaffolding project into ${root}...`);
+
+	const templateDir = path.resolve(
+		fileURLToPath(import.meta.url),
+		'../..',
+		`template-${useTypeScript ? 'ts' : 'js'}`
+	);
+
+	const write = (file: string, content?: string) => {
+		const targetPath = path.join(
+			root,
+			file === '_gitignore' ? '.gitignore' : file
+		);
+		content
+			? fs.writeFileSync(targetPath, content)
+			: copy(path.join(templateDir, file), targetPath);
+	};
+
+	const files = fs.readdirSync(templateDir);
+	for (const file of files) {
+		if (file === 'package.json') continue;
+		write(file);
+	}
+
+	const pkg = JSON.parse(
+		fs.readFileSync(path.join(templateDir, `package.json`), 'utf-8')
+	);
+	pkg.name = packageName || getProjectName();
+	write('package.json', JSON.stringify(pkg, null, 2));
+
+	console.log(
+		`\n${green('Success!')} Created vite react app at ${yellow(root)}:\n`
+	);
+	console.log(`Now run:\n`);
+	if (root !== cwd) {
+		console.log(`  cd ${path.relative(cwd, root)}`);
+	}
+	console.log('  npm install');
+	console.log('  npm run dev');
+	console.log();
+})().catch((e) => console.error(red(e)));
---
+++ src/utils.ts
@@ -0,0 +1,63 @@
+import fs from 'node:fs';
+import path from 'node:path';
+
+export function matchesPatterns(
+	str: string,
+	matchPatterns: (string | RegExp)[]
+) {
+	if (!Array.isArray(matchPatterns)) return false;
+	const patterns = matchPatterns
+		.filter((p) => p instanceof RegExp || typeof p === 'string')
+		.map((p) => (p instanceof RegExp ? p : new RegExp(p)));
+	return patterns.some((p) => p.test(str));
+}
+
+export function copy(
+	src: string,
+	dest: string,
+	excludePatterns?: (string | RegExp)[]
+) {
+	if (excludePatterns && matchesPatterns(src, excludePatterns)) return;
+	fs.statSync(src).isDirectory()
+		? copyDir(src, dest, excludePatterns)
+		: fs.copyFileSync(src, dest);
+}
+export function copyDir(
+	srcDir: string,
+	destDir: string,
+	excludePatterns?: (string | RegExp)[]
+) {
+	fs.mkdirSync(destDir, { recursive: true });
+	for (const file of fs.readdirSync(srcDir)) {
+		const srcFile = path.resolve(srcDir, file);
+		const destFile = path.resolve(destDir, file);
+		copy(srcFile, destFile, excludePatterns);
+	}
+}
+export function formatTargetDir(targetDir: string | undefined) {
+	return targetDir?.trim().replace(/\/+$/g, '') || '';
+}
+export function isEmpty(path: string) {
+	const files = fs.readdirSync(path);
+	return files.length === 0 || (files.length === 1 && files[0] === '.git');
+}
+export function emptyDir(dir: string) {
+	if (!fs.existsSync(dir)) return;
+	for (const file of fs.readdirSync(dir)) {
+		if (file === '.git') continue;
+		fs.rmSync(path.resolve(dir, file), { recursive: true, force: true });
+	}
+}
+export function isValidPackageName(projectName: string) {
+	return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(
+		projectName
+	);
+}
+export function toValidPackageName(projectName: string) {
+	return projectName
+		.trim()
+		.toLowerCase()
+		.replace(/\s+/g, '-')
+		.replace(/^[._]/, '')
+		.replace(/[^a-z0-9-~]+/g, '-');
+}
